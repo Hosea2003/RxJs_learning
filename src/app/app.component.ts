ebbb6af98bb9297144126bfe2dd7db07539f2769
Rindra Hosea MAHEFANIAINA <ranto@futurmap.com>
2022-12-07 12:03:06 +0300

    zip operators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subject, zip } from 'rxjs';
+import {tap} from 'rxjs/operators';
+
+type Durum=["flat bread", "meat", "sauce", "tomato", "cabbage"];
 
 @Component({
   selector: 'app-root',
@@ -9,7 +13,22 @@ export class AppComponent implements OnInit{
 
   title = 'RxJs';
 
+  durums!:Observable<Durum>;
+  _flatBread=new Subject<"flat bread">();
+  _meat=new Subject<"meat">();
+  _sauce=new Subject<"sauce">();
+  _tomato=new Subject<"tomato">();
+  _cabbage=new Subject<"cabbage">();
+
   ngOnInit(): void {
-    console.log("zip-operator");
+    this.durums=zip(
+      this._flatBread.pipe(tap(console.log)),
+      this._meat.pipe(tap(console.log)),
+      this._sauce.pipe(tap(console.log)),
+      this._tomato.pipe(tap(console.log)),
+      this._cabbage.pipe(tap(console.log))
+    ).pipe(
+      tap((durum)=>console.log("Enjoy!", durum))
+    );
   }
 }
