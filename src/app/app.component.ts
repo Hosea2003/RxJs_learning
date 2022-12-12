9064ffc2b01df6ab347e096fc3eebef39609dd5e
Rindra Hosea MAHEFANIAINA <mahefaniainarindra@gmail.com>
2022-12-12 12:20:01 +0300

    Scheduler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { asapScheduler, interval, Observable, observeOn, of, take, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'RxJs';
+
+  progress$!:Observable<number>;
+
+  ngOnInit(): void {
+    this.progress$=interval(1000/60).pipe(take(100));
+  }
+
+  runAsync(){
+    // assapScheduler, asyncScheduler
+    setTimeout(()=>console.log("setTimeout call back"), 0);
+    Promise.resolve("Promise value").then(console.log);
+    of("Stream value").pipe(
+      observeOn(asapScheduler)
+    ).subscribe(console.log);
+  }
 }
