6bc322d3eb9ee7103ce68b7f5ef57129866351bc
Rindra Hosea MAHEFANIAINA <mahefaniainarindra@gmail.com>
2022-12-12 14:19:08 +0300

    Hot and cold observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { map, Observable, of, shareReplay, timestamp } from 'rxjs';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'RxJs';
+
+  posts!:Observable<any[]>
+
+  constructor(private http:HttpClient){
+    // broadcasting the value to all subscriber (hot observable)
+    this.posts=this.http.get<any[]>("https://jsonplaceholder.typicode.com/posts").pipe(shareReplay())
+  }
+
+  ngOnInit(): void {
+    const obs=fromtimestamp();
+
+    obs.subscribe(console.log)
+
+    setTimeout(()=>{
+      obs.subscribe(console.log)
+    }, 2000)
+  }
+}
+
+const fromtimestamp=():Observable<number>=>{
+  //multicasting, same datasource shared for all subscriber
+  // const timestamp=Date.now()
+  return new Observable((subscriber)=>{
+    // unicast, each subscriber has its own datasource
+    const timestamp=Date.now()
+    subscriber.next(timestamp);
+  })
 }
